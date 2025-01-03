37fbbc942f0a2138ea5f5a7f512eae9a68902b98
Akgisno1 <anshulkumargodiwar2001@gmail.com>
2025-01-03 21:19:53 +0530

    clerk user fetch

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -56,7 +56,6 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
-    // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
       name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
